103aa27388d69bc5c84dd2eb5515b8975dc23475
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-06-06 15:12:56 +0700

    feat: fetch data from database

--- server.js
+++ server.js
@@ -4,17 +4,36 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import multer from 'multer';
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static('public'));
 
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'plastic_store',
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + '_' + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
 });
 
 con.connect(function (err) {
@@ -35,6 +54,34 @@ app.post('/login', (req, res) => {
     } else {
       return res.json({ Status: 'Error', Error: 'Wrong Email or Password' });
     }
+  });
+});
+
+app.get('/getEmployees', (req, res) => {
+  const sql = 'SELECT * FROM employee';
+  con.query(sql, (err, result) => {
+    if (err) return res.json({ Error: 'Get employee error in sql' });
+    return res.json({ Status: 'Success', Result: result });
+  });
+});
+
+app.post('/create', upload.single('image'), (req, res) => {
+  const sql =
+    'INSERT INTO employee (`name`,`email`,`password`, `address`, `salary`,`image`) VALUES (?)';
+  bcrypt.hash(req.body.password.toString(), 10, (err, hash) => {
+    if (err) return res.json({ Error: 'Error in hashing password' });
+    const values = [
+      req.body.name,
+      req.body.email,
+      hash,
+      req.body.address,
+      req.body.salary,
+      req.file.filename,
+    ];
+    con.query(sql, [values], (err, result) => {
+      if (err) return res.json({ Error: 'Inside signup query' });
+      return res.json({ Status: 'Success' });
+    });
   });
 });
 
